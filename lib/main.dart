211ceb09e78ff55c8268d8e7838418ee96050641
Artem <ap2774431@gmail.com>
2024-02-21 16:11:59 +0300

    TextField decoration

--- lib/main.dart
+++ lib/main.dart
@@ -1,114 +1,65 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
+import 'app.dart';
+
 
 void main(){
-  runApp(App());
+  runApp(Example());
 }
 
 
-class App extends StatefulWidget {
-  const App({super.key});
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  String text = "Ни одна кнопка не нажата";
-
-  void printFirst(){
-    setState(() {
-      text = "Нажата первая кнопка";
-    });
-  }
-
-  void printSecond(){
-    setState(() {
-      text = "Нажата вторая кнопка";
-    });
-  }
-
-  void printThird(){
-    setState(() {
-      text = "Нажата третья кнопка";
-    });
-  }
-
-  void printFourth(){
-    setState(() {
-      text = "Нажата четвертая кнопка";
-    });
-  }
+class Example extends StatelessWidget {
+  const Example({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final style = ButtonStyle(
-      //padding: MaterialStateProperty.all(EdgeInsets.all(0))
-      //backgroundColor: MaterialStateProperty.all(Colors.red),
-      //foregroundColor: MaterialStateProperty.all(Colors.white),
-      //overlayColor: MaterialStateProperty.all(Colors.redAccent),
-      //shadowColor: MaterialStateProperty.all(Colors.green),
-      //elevation: MaterialStateProperty.all(10),
-      //padding: MaterialStateProperty.all(EdgeInsets.all(0)),
-      //minimumSize: MaterialStateProperty.all(Size(100, 100)),
-      shape: MaterialStateProperty.all(
-        RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(0),
-          side: BorderSide(color: Colors.black)
-        )
-      ),
-      //alignment: Alignment.topCenter,
-
-    );
-    return Directionality(
-      textDirection: TextDirection.ltr,
-      child: Container(
-        color: Colors.black,
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(text),
-              ElevatedButton(
-                onPressed: printFirst,
-                style: style,
-                clipBehavior: Clip.hardEdge,
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Icon(Icons.ac_unit),
-                    SizedBox(width: 10,),
-                    Text("data"),
-                    SizedBox(width: 10,),
-                    Icon(Icons.ac_unit),
-                  ],
-                )
-              ),
-              OutlinedButton(
-                onPressed: printSecond,
-                child: Text(
-                    "Press me"
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(40),
+            child: TextField(
+              decoration: InputDecoration(
+                icon: Icon(Icons.snowboarding_outlined),
+                //labelText: "text",
+                helperText: "helper",
+                hintText: "enter",
+                //errorText: "error",
+                prefixIcon: Icon(Icons.alarm),
+                prefix: Text("prefix"),
+                suffix: Text("suffix"),
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.black,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(10)
                 ),
-                style: style,
-              ),
-              TextButton(
-                onPressed: printThird,
-                child: Text(
-                    "Press me"
+                disabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey,
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.circular(10)
                 ),
-                style: style,
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blue,
+                      width: 4,
+                    ),
+                    borderRadius: BorderRadius.circular(10)
+                ),
+                errorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.red,
+                      width: 4,
+                    ),
+                    borderRadius: BorderRadius.circular(10)
+                ),
+                contentPadding: EdgeInsets.symmetric(vertical: 0)
               ),
-              IconButton(
-                onPressed: printFourth,
-                icon: Icon(Icons.ac_unit),
-                iconSize: 48,
-                splashRadius: 300,
-                disabledColor: Colors.pink,
-                highlightColor: Colors.green,
-                splashColor: Colors.grey,
-              ),
-            ],
+            ),
           ),
         ),
       ),
