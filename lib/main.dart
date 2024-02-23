a857d56fbc035d09309754010800fe414c927989
Artem <ap2774431@gmail.com>
2024-02-23 12:47:41 +0300

    TextField управление

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:ui' as ui show BoxHeightStyle, BoxWidthStyle;
 
 import 'app.dart';
 
@@ -9,8 +11,58 @@ void main(){
 }
 
 
-class Example extends StatelessWidget {
-  const Example({super.key});
+class Example extends StatefulWidget {
+
+  Example({super.key});
+
+  @override
+  State<Example> createState() => _ExampleState();
+}
+
+class _ExampleState extends State<Example> {
+  final controllerOne = TextEditingController();
+  final controllerTwo = TextEditingController();
+  final nodeOne = FocusNode();
+  final nodeTwo = FocusNode();
+
+  void _onChanged(String text){
+    print("_onChanged $text");
+  }
+
+  void _onEditingComplete(){
+    print("_onEditingComplete");
+  }
+
+  void _onSubmitted(String text){
+    print("_onSubmitted $text");
+  }
+
+  void _onTap(){
+    print(nodeOne.hasFocus);
+    print(nodeTwo.hasFocus);
+    print("_onTap");
+  }
+
+  void _onAppPrivateCommand(String text, Map<String, dynamic> arguments){
+    print("_onAppPrivateCommand $text");
+    print("_onAppPrivateCommand $arguments");
+  }
+
+  void _onListen(){
+    controllerTwo.text = controllerOne.text.toUpperCase();
+  }
+
+  void onButtonTap(){
+    controllerOne.value = TextEditingValue(
+      text: "asdasdas",
+      selection: TextSelection.collapsed(offset: 0) // управление курсором,
+      // TextSelection(baseOffset, extentOffset) - выделение подстроки
+
+    );
+    controllerOne.addListener(_onListen);
+    controllerOne.removeListener(_onListen);
+    controllerTwo.clear();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,46 +71,64 @@ class Example extends StatelessWidget {
         body: Center(
           child: Padding(
             padding: const EdgeInsets.all(40),
-            child: TextField(
-              decoration: InputDecoration(
-                icon: Icon(Icons.snowboarding_outlined),
-                //labelText: "text",
-                helperText: "helper",
-                hintText: "enter",
-                //errorText: "error",
-                prefixIcon: Icon(Icons.alarm),
-                prefix: Text("prefix"),
-                suffix: Text("suffix"),
-                enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.black,
-                      width: 2,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  focusNode: nodeOne,
+                  controller: controllerOne,
+                  onChanged: _onChanged,
+                  onTap: _onTap,
+                  onEditingComplete: _onEditingComplete,
+                  onSubmitted: _onSubmitted,
+                  readOnly: false,
+                  //keyboardAppearance: Brightness.dark,
+                  //keyboardType: TextInputType.emailAddress,
+                  showCursor: true,
+                  cursorWidth: 2,
+                  //textCapitalization: TextCapitalization.sentences,
+                  //smartDashesType: SmartDashesType.enabled,
+                  //smartQuotesType: SmartQuotesType.disabled,
+                  //enableInteractiveSelection: true, // выделение в текстовом поле
+                  //toolbarOptions: ToolbarOptions(),
+                  //selectionHeightStyle: ui.BoxHeightStyle.max,
+                  //maxLines: null,
+                  //minLines: null,
+                  //expands: true,
+                  //maxLength: 10,
+                  //maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                  //textAlign: TextAlign.justify,
+
+
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.black,
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(10)
                     ),
-                    borderRadius: BorderRadius.circular(10)
-                ),
-                disabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.grey,
-                    width: 2,
                   ),
-                  borderRadius: BorderRadius.circular(10)
                 ),
-                focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.blue,
-                      width: 4,
+                SizedBox(height: 40,),
+                TextField(
+                  focusNode: nodeTwo,
+                  controller: controllerTwo,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.black,
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(10)
                     ),
-                    borderRadius: BorderRadius.circular(10)
+                  ),
                 ),
-                errorBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.red,
-                      width: 4,
-                    ),
-                    borderRadius: BorderRadius.circular(10)
-                ),
-                contentPadding: EdgeInsets.symmetric(vertical: 0)
-              ),
+                ElevatedButton(
+                  onPressed: onButtonTap,
+                  child: Icon(Icons.ac_unit),
+                )
+              ],
             ),
           ),
         ),
