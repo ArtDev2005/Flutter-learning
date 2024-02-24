d52233bd68f975caa452669fbf6a3f0284417eff
Artem <ap2774431@gmail.com>
2024-02-24 17:57:58 +0300

    TextField TextInputFormatter

--- lib/main.dart
+++ lib/main.dart
@@ -20,49 +20,6 @@ class Example extends StatefulWidget {
 }
 
 class _ExampleState extends State<Example> {
-  final controllerOne = TextEditingController();
-  final controllerTwo = TextEditingController();
-  final nodeOne = FocusNode();
-  final nodeTwo = FocusNode();
-
-  void _onChanged(String text){
-    print("_onChanged $text");
-  }
-
-  void _onEditingComplete(){
-    print("_onEditingComplete");
-  }
-
-  void _onSubmitted(String text){
-    print("_onSubmitted $text");
-  }
-
-  void _onTap(){
-    print(nodeOne.hasFocus);
-    print(nodeTwo.hasFocus);
-    print("_onTap");
-  }
-
-  void _onAppPrivateCommand(String text, Map<String, dynamic> arguments){
-    print("_onAppPrivateCommand $text");
-    print("_onAppPrivateCommand $arguments");
-  }
-
-  void _onListen(){
-    controllerTwo.text = controllerOne.text.toUpperCase();
-  }
-
-  void onButtonTap(){
-    controllerOne.value = TextEditingValue(
-      text: "asdasdas",
-      selection: TextSelection.collapsed(offset: 0) // управление курсором,
-      // TextSelection(baseOffset, extentOffset) - выделение подстроки
-
-    );
-    controllerOne.addListener(_onListen);
-    controllerOne.removeListener(_onListen);
-    controllerTwo.clear();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -72,33 +29,12 @@ class _ExampleState extends State<Example> {
           child: Padding(
             padding: const EdgeInsets.all(40),
             child: Column(
-              mainAxisSize: MainAxisSize.min,
               children: [
                 TextField(
-                  focusNode: nodeOne,
-                  controller: controllerOne,
-                  onChanged: _onChanged,
-                  onTap: _onTap,
-                  onEditingComplete: _onEditingComplete,
-                  onSubmitted: _onSubmitted,
-                  readOnly: false,
-                  //keyboardAppearance: Brightness.dark,
-                  //keyboardType: TextInputType.emailAddress,
-                  showCursor: true,
-                  cursorWidth: 2,
-                  //textCapitalization: TextCapitalization.sentences,
-                  //smartDashesType: SmartDashesType.enabled,
-                  //smartQuotesType: SmartQuotesType.disabled,
-                  //enableInteractiveSelection: true, // выделение в текстовом поле
-                  //toolbarOptions: ToolbarOptions(),
-                  //selectionHeightStyle: ui.BoxHeightStyle.max,
-                  //maxLines: null,
-                  //minLines: null,
-                  //expands: true,
-                  //maxLength: 10,
-                  //maxLengthEnforcement: MaxLengthEnforcement.enforced,
-                  //textAlign: TextAlign.justify,
-
+                  inputFormatters: [
+                    PhoneInputFormatter(),//регулярные выражения
+                    //FilteringTextInputFormatter.allow(RegExp(r'[/d]+')),
+                  ],
 
                   decoration: InputDecoration(
                     border: OutlineInputBorder(
@@ -110,24 +46,6 @@ class _ExampleState extends State<Example> {
                     ),
                   ),
                 ),
-                SizedBox(height: 40,),
-                TextField(
-                  focusNode: nodeTwo,
-                  controller: controllerTwo,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.black,
-                          width: 2,
-                        ),
-                        borderRadius: BorderRadius.circular(10)
-                    ),
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: onButtonTap,
-                  child: Icon(Icons.ac_unit),
-                )
               ],
             ),
           ),
@@ -136,3 +54,37 @@ class _ExampleState extends State<Example> {
     );
   }
 }
+
+
+class PhoneInputFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(TextEditingValue oldValue,
+      TextEditingValue newValue) {
+    final digitsOnly = newValue.text.replaceAll(RegExp(r'[^\d]+'), ''); // заменяет все что не удовлетворяет условию
+    final initialSpecialSymbolCount = newValue.selection
+        .textBefore(newValue.text)
+        .replaceAll(RegExp(r'[\d]+'), '')
+        .length;
+    final cursorPosition = newValue.selection.start - initialSpecialSymbolCount;
+    print(cursorPosition);
+    var finalCursorPosition = cursorPosition;
+    final digitsOnlyChars = digitsOnly.split('');
+    var newString = <String>[];
+    for (var i = 0; i < digitsOnlyChars.length; i++){
+      if (i == 3 || i == 6 || i == 8) {
+        newString.add(' ');
+        if (i <= cursorPosition) finalCursorPosition++;
+      }
+      newString.add(digitsOnlyChars[i]);
+
+    }
+    final resultString = newString.join('');
+    print("final $finalCursorPosition");
+    print("result string $resultString");
+    return TextEditingValue(
+        text: resultString,
+        selection: TextSelection.collapsed(offset: finalCursorPosition) // положение курсора
+    );
+  }
+}
+
