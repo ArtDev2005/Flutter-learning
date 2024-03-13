3396e35d762720456ef85884206bebf6804e422f
Artem <ap2774431@gmail.com>
2024-03-13 15:58:26 +0300

    Http запросы 2

--- lib/domain/api_clients/api_clients.dart
+++ lib/domain/api_clients/api_clients.dart
@@ -16,4 +16,24 @@ class ApiClient{
     final posts = json.map((dynamic e) => Post.fromJson(e as Map<String, dynamic>)).toList();
     return posts;
   }
+
+  Future<Post> createPost({required String title, required String body}) async {
+    final url = Uri.parse('https://jsonplaceholder.typicode.com/posts');
+    final parameters = <String, dynamic>{
+      'title': title,
+      'body': body,
+      'userId': 109
+    };
+    final request = await client.postUrl(url);
+    request.headers.add('Content-type', 'application/json; charset=UTF-8');
+    request.write(jsonEncode(parameters));
+    final response = await request.close();
+    final string = await response
+        .transform(utf8.decoder)
+        .toList()
+        .then((value) => value.join());
+    final json = jsonDecode(string) as Map<String, dynamic>;
+    final post = Post.fromJson(json);
+    return post;
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -6,45 +6,101 @@ import 'package:flutter_learning/example_model.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 void main(){
-  const app = MyApp();
-  runApp(app);
+  runApp(Example());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class Example extends StatefulWidget {
+  Example({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  _ExampleState createState() => _ExampleState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _ExampleState extends State<Example> {
   final model = ExampleWidgetModel();
+
   @override
   Widget build(BuildContext context) {
-    ApiClient().getPosts();
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
           child: ExampleModelProvider(
             model: model,
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                ElevatedButton(
-                  child: Text("Update posts"),
-                  onPressed: () => model.reloadPosts(),
+                const _ReloadButton(),
+                const _CreateButton(),
+                const Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: _PostsWidget(),
+                  ),
                 ),
-                SizedBox(height: 20,),
-                ElevatedButton(
-                  child: Text("Create posts"),
-                  onPressed: () => model.createPosts(),
-                )
               ],
             ),
           ),
-        )
+        ),
       ),
     );
   }
 }
 
+class _ReloadButton extends StatelessWidget {
+  const _ReloadButton({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () => ExampleModelProvider.read(context)?.model.reloadPosts(),
+      child: const Text('Обновить посты'),
+    );
+  }
+}
+
+class _CreateButton extends StatelessWidget {
+  const _CreateButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () => ExampleModelProvider.read(context)?.model.createPosts(),
+      child: const Text('Создать пост'),
+    );
+  }
+}
+
+class _PostsWidget extends StatelessWidget {
+  const _PostsWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: ExampleModelProvider.watch(context)?.model.posts.length ?? 0,
+      itemBuilder: (BuildContext context, int index) {
+        return _PostsRowWidget(index: index);
+      },
+    );
+  }
+}
+
+class _PostsRowWidget extends StatelessWidget {
+  final int index;
+  const _PostsRowWidget({Key? key, required this.index}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final post = ExampleModelProvider.read(context)!.model.posts[index];
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(post.id.toString()),
+        const SizedBox(height: 10),
+        Text(post.title),
+        const SizedBox(height: 10),
+        Text(post.body),
+        const SizedBox(height: 40),
+      ],
+    );
+  }
+}
